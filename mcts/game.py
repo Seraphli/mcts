aa1b704b0c0790d6498307343311a217640bfdf2
SErAphLi <seraphlivery@gmail.com>
2019-03-27 11:09:13 +0800

    Update comment

--- mcts/game.py
+++ mcts/game.py
@@ -1,17 +1,44 @@
 class Game(object):
+    """Game template class."""
+
     def __init__(self):
+        """player_just_moved is required."""
         self.player_just_moved = 2
 
     def clone(self):
+        """Clone the game state for searching.
+
+        Returns:
+            the cloned game state.
+
+        """
         pass
 
     def take_action(self, action):
+        """Update the game state by carrying out the given action.
+
+        Args:
+            action: the given action.
+
+        """
         pass
 
     def get_actions(self):
+        """Get all possible actions from this state.
+
+        Returns:
+            all possible actions.
+
+        """
         pass
 
     def get_result(self, playerjm):
+        """Get the game result from the viewpoint of playerjm.
+
+        Args:
+            playerjm: the player that take the action
+
+        """
         pass
 
     def __repr__(self):
---
+++ setup.py
@@ -0,0 +1,23 @@
+import re
+from setuptools import setup, find_packages
+
+
+# Read property from project's package init file
+def get_property(prop, project):
+    result = re.search(r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop),
+                       open(project + '/__init__.py').read())
+    return result.group(1)
+
+
+setup(
+    name='mcts',
+    version=get_property('__version__', 'mcts'),
+    description='MCTS package',
+    author='SErAphLi',
+    url='https://github.com/Seraphli/mcts',
+    license='MIT License',
+    packages=find_packages(),
+    install_requires=[
+        'tqdm'
+    ]
+)
